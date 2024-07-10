c0c45c065a37b3b3f511ea9e74a69bb1d46b4a42
wellmota <impeto.wellington@gmail.com>
2024-07-10 12:09:22 -0300

    finished exercise 3

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -37,4 +37,56 @@ describe("Calculator", () => {
     const result = screen.getByTestId("result")
     expect(result).toHaveTextContent("4")
   })
+
+  it("should subtract numbers", () => {
+    render(<Home />)
+    const num1Input = screen.getByTestId("num1")
+    const num2Input = screen.getByTestId("num2")
+    const subtractBtn = screen.getByTestId("subtract")
+
+    act(() => {
+      fireEvent.change(num1Input, { target: { value: 3 } })
+      fireEvent.change(num2Input, { target: { value: 1 } })
+      subtractBtn.click()
+    })
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("2")
+  })
+
+
+  it("should multiply numbers", () => {
+    render(<Home />)
+    const num1Input = screen.getByTestId("num1")
+    const num2Input = screen.getByTestId("num2")
+    const multiplyBtn = screen.getByTestId("multiply")
+
+    act(() => {
+      fireEvent.change(num1Input, { target: { value: 3 } })
+      fireEvent.change(num2Input, { target: { value: 2 } })
+      multiplyBtn.click()
+    })
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("6")
+  })
+
+
+  it("should divide numbers", () => {
+    render(<Home />)
+    const num1Input = screen.getByTestId("num1")
+    const num2Input = screen.getByTestId("num2")
+    const divideBtn = screen.getByTestId("divide")
+
+    act(() => {
+      fireEvent.change(num1Input, { target: { value: 4 } })
+      fireEvent.change(num2Input, { target: { value: 2 } })
+      divideBtn.click()
+    })
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("2")
+  })
+
+
 })

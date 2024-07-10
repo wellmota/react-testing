b8fbabb079078d9708a40ec8b0aa7d87fe0e7be4
wellmota <impeto.wellington@gmail.com>
2024-07-10 11:24:42 -0300

    finish exercise 1

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -7,6 +7,12 @@ describe("Calculator", () => {
   it("should render the calculator", () => {
     render(<Home />)
     expect(screen.getByTestId("result")).toBeInTheDocument()
+    expect(screen.getByTestId("num1")).toBeInTheDocument()
+    expect(screen.getByTestId("num2")).toBeInTheDocument()
+    expect(screen.getByTestId("add")).toBeInTheDocument()
+    expect(screen.getByTestId("subtract")).toBeInTheDocument()
+    expect(screen.getByTestId("multiply")).toBeInTheDocument()
+    expect(screen.getByTestId("divide")).toBeInTheDocument()
   })
 
   it("should add numbers", () => {

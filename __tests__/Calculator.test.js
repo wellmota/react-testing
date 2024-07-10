764a7082413b5a6b4f92d0532dffaf495658b24f
wellmota <impeto.wellington@gmail.com>
2024-07-10 16:44:59 -0300

    finished exercise 6

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,30 +1,37 @@
-import Home from "../src/app/page";
-import { fireEvent, render, screen, act } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import Home from "../src/app/page"
+import { fireEvent, render, screen, act } from "@testing-library/react"
+import "@testing-library/jest-dom"
+
 
 beforeEach(() => {
-  render(<Home />);
-});
+  render(<Home />)
+})
 
 const elementShouldRender = (element, label) => {
-  const elementNode = screen.getByTestId(element);
-  expect(elementNode).toBeInTheDocument();
+  const elementNode = screen.getByTestId(element)
+  expect(elementNode).toBeInTheDocument()
   if (label !== undefined) {
-    expect(elementNode).toHaveTextContent(label);
+    expect(elementNode).toHaveTextContent(label)
   }
-};
+}
 
 const inputNumberExecution = (n1, n2, operation) => {
-  const num1Input = screen.getByTestId("num1");
-  const num2Input = screen.getByTestId("num2");
-  const operationBtn = screen.getByTestId(operation);
+  const num1Input = screen.getByTestId("num1")
+  const num2Input = screen.getByTestId("num2")
+  const operationBtn = screen.getByTestId(operation)
 
   act(() => {
-    fireEvent.change(num1Input, { target: { value: n1 } });
-    fireEvent.change(num2Input, { target: { value: n2 } });
-    operationBtn.click();
-  });
-};
+    fireEvent.change(num1Input, { target: { value: n1 } })
+    fireEvent.change(num2Input, { target: { value: n2 } })
+    operationBtn.click()
+  })
+}
+
+const randomNumbers = () => {
+  const num1 = Math.floor(Math.random() * 100)
+  const num2 = Math.floor(Math.random() * 100)
+  return { num1, num2 }
+}
 
 describe("Calculator", () => {
   it("should render the calculator components within correct labels", () => {
@@ -39,35 +46,69 @@ describe("Calculator", () => {
     }
 
     Object.entries(elementsOnPage).forEach(([element, label]) => {
-      elementShouldRender(element, label);
-    });
-  });
+      elementShouldRender(element, label)
+    })
+  })
 
   it("should add numbers", () => {
-    inputNumberExecution(2, 2, "add");
+    inputNumberExecution(2, 2, "add")
 
-    const result = screen.getByTestId("result");
-    expect(result).toHaveTextContent("4");
-  });
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("4")
+  })
 
   it("should subtract numbers", () => {
-    inputNumberExecution(2, 2, "subtract");
+    inputNumberExecution(2, 2, "subtract")
 
-    const result = screen.getByTestId("result");
-    expect(result).toHaveTextContent("0");
-  });
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("0")
+  })
 
   it("should multiply numbers", () => {
-    inputNumberExecution(2, 2, "multiply");
+    inputNumberExecution(2, 2, "multiply")
 
-    const result = screen.getByTestId("result");
-    expect(result).toHaveTextContent("4");
-  });
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("4")
+  })
 
   it("should divide numbers", () => {
-    inputNumberExecution(2, 2, "divide");
+    inputNumberExecution(2, 2, "divide")
 
-    const result = screen.getByTestId("result");
-    expect(result).toHaveTextContent("1");
-  });
-});
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent("1")
+  })
+})
+
+describe("Calculator with random numbers", () => {
+  const randomTestNumbers = randomNumbers()
+
+  it("should add random numbers", () => {
+    inputNumberExecution(randomTestNumbers.num1, randomTestNumbers.num2, "add")
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent(
+      randomTestNumbers.num1 + randomTestNumbers.num2
+    )
+  })
+
+  it("should subtract random numbers", () => {
+    inputNumberExecution(randomNumbers.num1, randomNumbers.num2, "subtract")
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent(randomNumbers.num1 - randomNumbers.num2)
+  })
+
+  it("should multiply random numbers", () => {
+    inputNumberExecution(randomNumbers.num1, randomNumbers.num2, "multiply")
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent(randomNumbers.num1 * randomNumbers.num2)
+  })
+
+  it("should divide random numbers", () => {
+    inputNumberExecution(randomNumbers.num1, randomNumbers.num2, "divide")
+
+    const result = screen.getByTestId("result")
+    expect(result).toHaveTextContent(randomNumbers.num1 / randomNumbers.num2)
+  })
+}) 
